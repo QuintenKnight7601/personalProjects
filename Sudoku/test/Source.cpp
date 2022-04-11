62718bfa2f0dc24d3d735fdd16f90b154486c95d
QuintenKnight7601 <QuintenKnight7601@gmail.com>
2022-04-11 16:13:45 -0600

    update files

--- Sudoku/test/Source.cpp
+++ Sudoku/test/Source.cpp
@@ -5,30 +5,29 @@ using namespace std;
 
 int main ( )
 {
+    //Defining variables
     sudoku puzzle;
-    ifstream fin("..\\test.txt");
-    vector<int> temp = { 1, 2, 3, 4,
-                         3, 4, 1, 2,
-                         2, 1, 4, 3,
-                         4, 3, 2, 0 };
+    int selection;
+
+    selection = menu ( puzzle );
+    
+}
 
 
-    puzzle.resize ( 9 );
-    puzzle.puzzIn ( fin );
-    puzzle.solve ( 1 );
-    temp = puzzle.cont ( 1 );
+int menu ( sudoku puzzle )
+{
+    //Defining variables
+    int selection;
 
-    for ( int i = 0; i < 81; ++i )
-    {
-        cout << temp.at ( i );
-        
-        if ( ( i % 9 ) == 8 )
-        {
-            cout << endl;
-        }
-        else
-        {
-            cout << " ";
-        }
-    }
+    cout << "---Sudoku DLL tester---";
+    cout << "\n\n";
+    cout << "  Puzzle size [" << puzzle.size ( ) << ", " << puzzle.size ( );
+    cout << "]\n\n";
+    cout << "  1) export template\n";
+    cout << "  2) import puzzle file (must match size of puzzle)\n";
+    cout << "  3) resize puzzle (sqrt of size must be an integer)\n";
+    cout << "  4) solve puzzle\n\n";
+
+    cin.getline ( (char*) selection );
+
 }
