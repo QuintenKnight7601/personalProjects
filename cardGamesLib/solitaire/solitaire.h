1100561209d01fc1e015db57791f69fe9932da14
QuintenKnight7601 <QuintenKnight7601@gmail.com>
2022-09-14 13:03:40 -0600

    cardGamesLib - solitaire - building class

--- cardGamesLib/solitaire/solitaire.h
+++ cardGamesLib/solitaire/solitaire.h
@@ -6,10 +6,16 @@
 #define __SOLITAIRE__H__
 
 class solitaire : protected cards {
+    cards tableau[7];
+    cards foundations[4];
+    cards draw;
+    cards discard;
 
 public:
+    //Con/Deconstructors
     solitaire(cards deck);
     solitaire(bool fill = false);
+    ~solitaire();
 };
 
 
