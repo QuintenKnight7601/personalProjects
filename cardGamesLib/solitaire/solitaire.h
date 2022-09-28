48417edac055db7d219c3b5b1edccc93be7e9908
Quinten David Knight <QuintenDavid.Knight@mines.sdsmt.edu>
2022-09-28 10:44:01 -0600

    cardGamesLib - solitaire

--- cardGamesLib/solitaire/solitaire.h
+++ cardGamesLib/solitaire/solitaire.h
@@ -10,30 +10,81 @@ class solitaire : protected cards {
         CARD::suits suit;
 
     public:
-        foundationClass(CARD::suits value = CARD::suits()) {
-            suit = value;
+        //Con/Deconstructors
+        foundationClass(char value = 'n') {
+            suit = CARD::fromChar(value);
         }
         ~foundationClass() {}
 
-        inline bool change(CARD::suits value) { suit = value; return true; };
+        //Information
+        inline bool valid(int num, bool reverse = false) {
+            int temp = peek().val;
+
+            //If order is reversed
+            if (reverse) {
+                for (int i = 1; i < num; ++i) {
+                    if (peek(i).val != (temp - 1)) {
+                        return false;
+                    }
+                }
+            }
+            //Order is foreword
+            else {
+                for (int i = 1; i < num; ++i) {
+                    if (peek(i).val != (temp + 1)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        //Modifications
+        inline bool modSuit(CARD::suits value) { 
+            suit = value; 
+            return true;
+        }
+        inline bool place(CARD::_card cardIn, int pos = 0, bool reverse = false) { 
+            if (cardIn.suit == suit)
+                return cards::place(cardIn, pos, reverse);
+            return false;
+        }
+        inline bool move(foundationClass dest, int num) {
+            stack<card> temp;
+            if (valid(num)) {
+                for (int i = 0; i < num; ++i) {
+                    temp.push(draw());
+                }
+                if (dest.peek().val == (temp.top().val + 1)) {
+                    while (!temp.empty()) {
+                        dest.place(temp.top());
+                        temp.pop();
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
     };
     class tableauClass:public cards {
     public:
-        tableauClass();
-        ~tableauClass();
+        //Con/Deconstructors
+        tableauClass() {};
+        ~tableauClass() {};
     };
 
     tableauClass tableau[7];
-    foundationClass clubFoun(CARD::suits::club);
-    foundationClass diamFoun(CARD::suits::diamond);
-    foundationClass hearFoun(CARD::suits::heart);
-    foundationClass spadFoun(CARD::suits::spade);
+    foundationClass foundation[4];
     cards discard;
     cards moving;
 
 public:
     //Con/Deconstructors
-    solitaire(cards deck);
+    solitaire(cards deck) { 
+        foundation[0].modSuit(CARD::suits::club);
+        foundation[1].modSuit(CARD::suits::diamond);
+        foundation[2].modSuit(CARD::suits::heart);
+        foundation[3].modSuit(CARD::suits::spade);
+    };
     solitaire(bool prepare = false);
     ~solitaire();
 
