6b844c09a68a33a95bb469e55f55e3c33a2fc201
Quinten David Knight <QuintenDavid.Knight@mines.sdsmt.edu>
2022-10-05 14:11:37 -0600

    temp Team2 project for school

---
+++ Team2/Team2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Team2.Data;
+
+[assembly: HostingStartup(typeof(Team2.Areas.Identity.IdentityHostingStartup))]
+namespace Team2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Team2Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Team2ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Team2Context>();
+            });
+        }
+    }
+}
